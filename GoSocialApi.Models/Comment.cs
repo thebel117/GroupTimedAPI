e4f1ffc76c9db8e96768ea732f9850bac7d01cbc
Tanner Hebel <thebel117@gmail.com>
2024-06-12 18:51:06 -0400

    Small fixes

--- GoSocialApi.Models/Comment.cs
+++ GoSocialApi.Models/Comment.cs
@@ -3,9 +3,9 @@ namespace GoSocialApi.Models
     public class Comment
     {
         public int Id { get; set; }
-        public string Text { get; set; }
+        public string? Text { get; set; }
         public int AuthorId { get; set; }
         public int PostId { get; set; }
-        public virtual List<Reply> Replies { get; set; }
+        public virtual List<Reply>? Replies { get; set; }
     }
 }
---
+++ GoSocialApi.Services/UserService.cs
@@ -0,0 +1,24 @@
+using GoSocialApi.Models;
+
+namespace GoSocialApi.Data
+{
+    public class UserService : IUserService
+    {
+        private readonly IUserRepository _userRepository;
+
+        public UserService(IUserRepository userRepository)
+        {
+            _userRepository = userRepository;
+        }
+
+        public IEnumerable<User> GetAllUsers() => _userRepository.GetAllUsers();
+
+        public User GetUserById(int id) => _userRepository.GetUserById(id);
+
+        public void CreateUser(User user) => _userRepository.CreateUser(user);
+
+        public void UpdateUser(User user) => _userRepository.UpdateUser(user);
+
+        public void DeleteUser(int id) => _userRepository.DeleteUser(id);
+    }
+}
